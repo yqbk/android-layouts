014dc41476332c7e74f72fe99773b79f3de6136f
mateuszwosik <mateusz.wosik@gmail.com>
2016-11-22 17:24:01 +0100

    done

---
+++ app/src/main/java/mw/relativelayoutexample/MainActivity.java
@@ -0,0 +1,13 @@
+package mw.relativelayoutexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
